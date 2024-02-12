e8b075b48078c2844f6d4e1c6a2eababf373fa9e
SathvikaNarla11 <sathvikanarla2002@gmail.com>
2024-02-12 18:52:36 +0530

    C Assignment

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    printf("The value is: %d\n", num);
+    printf("Hexadecimal number is: 0x%x\n", num);
+    printf("Octal number is: 0%o\n", num);
+
+}
